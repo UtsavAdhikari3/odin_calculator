357c313f1fb9c52c6ba869bb073fd6a7fad02ee6
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-09-04 13:43:35 +0545

    added simple operation and some buttons

---
+++ script.js
@@ -0,0 +1,37 @@
+let operand1 = 0;
+let operator = "";
+let operand2 = 0;
+
+
+function add(operand1,operand2){
+    return operand1 + operand2;
+}
+
+function subtract(operand1, operand2){
+    return operand1 - operand2;
+}
+
+function multiply(operand1, operand2){
+    return operand1 * operand2;
+}
+
+function divide(operand1, operand2){
+    return (operand1 / operand2).toFixed(2);
+}
+
+function operate(operand1, operand2){
+    switch(operator){
+        case "+":
+            add(operand1,operand2);
+            break;
+        case "-":
+            subtract(operand1,operand2);
+            break;
+        case "*":
+            multiply(operand1,operand2);
+            break;
+        case "/":
+            divide(operand1,operand2);
+            break;
+    }
+}
